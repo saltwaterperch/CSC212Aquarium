95e6960f02cc36f7623df7b1cd094c8d4c6411d8
owenpertsch <owenpertsch@131.229.149.120>
2019-09-13 15:11:24 -0400

    End of lab updates

--- src/main/java/edu/smith/cs/csc212/aquarium/Fish.java
+++ src/main/java/edu/smith/cs/csc212/aquarium/Fish.java
@@ -8,25 +8,47 @@ public class Fish {
 	int x;
 	int y;
 	Color color;
-	// choose which method to call from DrawFish...Fish...()
-	// boolean facingLeft;
-	// boolean isLittle;
+	boolean facingLeft;
+	boolean isLittle;
 
 	// constructor: construct a fish at position with color
-public Fish(Color color, int startX, int startY ) {
-		// boolean facingLeft, boolean isLittle) {
+public Fish(Color color, int startX, int startY,  
+		boolean facingLeft, boolean isLittle) 
+{
 	this.x = startX;
 	this.y = startY;
 	this.color = color;
+	// no need for these here?
+	this.facingLeft = facingLeft;
+	this.isLittle = isLittle;
 	
 	
 }
-
+	// nest if statements
 	public void draw(Graphics2D window) {
 		this.swim();
-		DrawFish.facingRight(window, 
-				this.color, this.x, this.y);
+		
+		// if its small, go through small draw and check for 
+		// which way it's facing
+		if ( this.isLittle) {
+			if (this.facingLeft) {
+			DrawFish.smallFacingRight(window, this.color,
+					this.x, this.y);
+			DrawFish.smallFacingLeft(window, this.color,
+					this.x, this.y);
+		} } else {
+			// if fish is big
+			DrawFish.facingLeft(window,
+					this.color, this.x, this.y);
+			DrawFish.facingLeft(window,
+					this.color, this.x, this.y);
+
+		}
+	
+		
 	}
+	
+	
 
 	public void swim() {
 		this.y += 1;
